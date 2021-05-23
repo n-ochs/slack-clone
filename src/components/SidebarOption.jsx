2d8e47ad3d2a6842c8f4e2089a91f052d7571cb1
n-ochs <nick.ochs@yahoo.com>
2021-05-23 19:42:32 -0400

    adds redux setup

--- src/components/SidebarOption.jsx
+++ src/components/SidebarOption.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { db } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { enterRoom } from '../features/appSlice';
 
 function SidebarOption({ Icon, title, addChannelOption, id }) {
+
+    const dispatch = useDispatch();
 
     const addChannel = () => {
         const channelName = prompt('Please enter the channel name.');
@@ -15,7 +19,11 @@ function SidebarOption({ Icon, title, addChannelOption, id }) {
     };
 
     const selectChannel = () => {
-        
+        if (id) {
+            dispatch(enterRoom({
+                roomId: id
+            }));
+        };
     };
 
     return (
