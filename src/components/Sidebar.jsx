4d81110c8175fcfb5cd670d35c65e23076f9191b
n-ochs <nick.ochs@yahoo.com>
2021-05-23 19:13:28 -0400

    adds SidebarInfo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
-
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import { db } from '../firebase';
+import { useCollection } from 'react-firebase-hooks/firestore';
 
 function Sidebar() {
+
+    const [ channels, loading, error ] = useCollection(db.collection('rooms'));
+
     return (
         <SidebarContainer>
             <SidebarHeader>
@@ -17,6 +32,28 @@ function Sidebar() {
                 </SidebarInfo>
                 <CreateIcon />
             </SidebarHeader>
+
+            <SidebarOption Icon={InsertCommentIcon} title='Threads' />
+            <SidebarOption Icon={InboxIcon} title='Mentions & Reactions' />
+            <SidebarOption Icon={DraftsIcon} title='Saved Items' />
+            <SidebarOption Icon={BookmarkBorderIcon} title='Channel Browser' />
+            <SidebarOption Icon={PeopleAltIcon} title='People & User Groups' />
+            <SidebarOption Icon={AppsIcon} title='Apps' />
+            <SidebarOption Icon={FileCopyIcon} title='File Browser' />
+            <SidebarOption Icon={ExpandLessIcon} title='Show less' />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title='Channels' />
+            <hr />
+            <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel' />
+
+            {channels?.docs.map((doc) => (
+                <SidebarOption 
+                    key={doc.id} 
+                    id={doc.id}  
+                    title={doc.data().name} 
+                />
+            ))}
+
         </SidebarContainer>
     );
 };
@@ -30,6 +67,12 @@ const SidebarContainer = styled.div`
     border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b
+    }
 `;
 
 const SidebarHeader = styled.div`
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 require('dotenv').config();
 
 const firebaseConfig = {
@@ -8,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
