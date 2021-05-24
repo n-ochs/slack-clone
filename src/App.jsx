1e893ab564dd2b0961933314b9e3404b40de98f0
n-ochs <nick.ochs@yahoo.com>
2021-05-23 20:43:37 -0400

    adds chat box

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Chat from './components/Chat';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
           <Sidebar />
           <Switch>
             <Route path="/" exact>
-              {/* Chat */}
+              <Chat />
             </Route>
           </Switch>
         </AppBody>
